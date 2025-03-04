8a624d0c4a2ca03b9e322360e7eed089b338cfeb
Alejandro Garcia <145106211+aalexgarciagl@users.noreply.github.com>
2025-03-04 10:11:30 +0100

    politica de coockies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { TratamientosComponent } from './componentes/tratamientos/tratamientos.c
 import { PreguntasComponent } from './componentes/preguntas/preguntas.component';
 import { TerapiasComponent } from './componentes/terapias/terapias.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { PoliticaCookiesComponent } from './politica-cookies/politica-cookies.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/inicio' },
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'tratamientos', component: TratamientosComponent },
     { path: 'preguntas', component: PreguntasComponent },
     { path: 'terapias', component: TerapiasComponent },
+    { path: 'politica-cookies', component: PoliticaCookiesComponent },
 ];
